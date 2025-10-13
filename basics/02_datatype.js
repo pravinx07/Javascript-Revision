30b02f8eda560c9c0e856d45b76b35fd99953b28
Pravin Shegamwar <156991992+pravinx07@users.noreply.github.com>
2025-10-13 01:44:35 +0000

    learn datatypes and ECMA standards  in Javascript

---
+++ basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use-strict"  // treat all JS code as newer version
+
+// alert("Hello")   // we are using nodejs not browser
+
+// console.log(0.2 + 0.1)  // gives => 0.30000000000000004
+
+let name = "Pravin"  // string
+let age = 23  // age
+
+
+// Primitive data type
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable declared but value not assign
+// symbol => unique
+
+
+// Object
+console.log(typeof undefined);  // its undefined
+console.log(typeof null); // its give object 
+
+
+
+
+
+
+
