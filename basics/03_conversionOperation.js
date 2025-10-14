8f0ac075a1185ab5f8acda3667a14e7a02328cae
MrPravinS <shegamwarpravin07@gmail.com>
2025-10-14 07:04:00 +0530

    leran number conversion in Javascript

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "33abc";
+
+console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let newValueInNumber = Number(score)
+// console.log(typeof newValueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; flase => 0
+
+let isLoggedIn = ""
+
+let booleanVal = Boolean(isLoggedIn)
+// console.log(booleanVal);
+
+
+// 1, [], {}, "hello" => true
+// 0 => false
+// "" => false ,
+
+
+let number = 22
+
+let strNumber = String(number)
+console.log(strNumber);
+console.log( typeof strNumber);
+
+
+// 22 => "22"
+// type string
