ff0a1f29b37af1977255313fd167c18ca5da8704
MrPravinS <shegamwarpravin07@gmail.com>
2025-10-15 07:17:51 +0530

    learn string conversion , writing clean code

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -1,6 +1,8 @@
+const e = require("express");
+
 let score = "33abc";
 
-console.log(score);
+// console.log(score);
 // console.log(typeof score);
 // console.log(typeof(score));
 
@@ -24,12 +26,55 @@ let booleanVal = Boolean(isLoggedIn)
 // "" => false ,
 
 
-let number = 22
+// let number = 22
 
-let strNumber = String(number)
-console.log(strNumber);
-console.log( typeof strNumber);
+// let strNumber = String(number)
+// console.log(strNumber);
+// console.log( typeof strNumber);
 
 
 // 22 => "22"
 // type string
+
+
+// ************* Operations ****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 3);
+
+
+// let str1 = "hello"
+// let str2 = " pravin"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+//  console.log(+true);
+//  console.log(+"");
+
+ let num1,num2,num3;
+ num1 = num2 = num3 = 3 + 3
+ 
+ 
+ let gameCounter = 100
+//  gameCounter++
+ ++gameCounter
+ console.log(gameCounter);
+ 
+
+
+let x = 3
+let y = x++
+let z = ++x
+
+// console.log(x  , z);
+
+
