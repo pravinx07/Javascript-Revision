1b139bb1611445e830361516e66c60879d8306cc
MrPravinS <shegamwarpravin07@gmail.com>
2025-10-19 03:45:45 +0530

    learn about string in javascript

---
+++ basics/06.string.js
@@ -0,0 +1,34 @@
+const name = "Pravin";
+const repoCount = 25
+
+// console.log(`hello my name is ${name} and repo count is ${repoCount}`);
+
+
+const getName = String("Pravin")
+
+// console.log(getName[0]);
+// console.log(getName.__proto__);
+
+// console.log(getName.length);
+// console.log(getName.toUpperCase());
+// console.log(getName.charAt(2));
+// console.log(getName.indexOf("a"));
+
+const newStr = getName.substring(0,4)
+// console.log(newStr);
+
+const anotherStr = getName.slice(-8,5)
+// console.log(anotherStr);
+
+const url = "https://pravin.com/pravin%20com"
+
+// console.log(url.replace("%20","-"));
+// console.log(url.includes("summry"));
+let aname = "pravin S "
+console.log(aname.split("-"));
+
+
+
+
+
+
