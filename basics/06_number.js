e294fd4387cac0e4673662d3a79b4a7a882e66d7
MrPravinS <shegamwarpravin07@gmail.com>
2025-10-20 06:51:06 +0530

    learn about numbers and Math in js

---
+++ basics/06_number.js
@@ -0,0 +1,45 @@
+const score = 300;
+let balance = new Number(score)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+
+let otherNumber = 123.8922
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+// +++++++++++++++++++ Math ++++++++++++++++
+
+// console.log(Math);
+
+// convert neg to pos
+// console.log(Math.abs(-4));
+
+
+// console.log(Math.round(4.9));
+
+// give upper value
+// console.log(Math.ceil(4.7));
+
+// give floor value
+// console.log(Math.floor(4.7));
+
+// console.log(Math.max(3,1,4,1,4));
+// console.log(Math.min(3,1,4,1,4));
+
+
+// between 0 to 1
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log((Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
