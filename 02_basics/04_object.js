9c43ced8b05e6fcba29ef61f395ef44472cdde3d
MrPravinS <shegamwarpravin07@gmail.com>
2025-10-25 07:16:14 +0530

    learn about singleton objects and some objects methods

---
+++ 02_basics/04_object.js
@@ -0,0 +1,84 @@
+// singleton object
+// const tinderObject = new Object();
+
+// non singleton object
+const tinderUser = {}
+
+tinderUser.id = "abc";
+tinderUser.name = "john"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser);
+
+
+const regulerUser = {
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Pravin",
+            lastname:"patil"
+        }
+    }
+}
+
+
+// ?  optinal chaining  to check nested object is valu exist or not
+// console.log(regulerUser.fullname?.userfullname.firstname);
+
+
+const obj1 = {
+    1:"a",
+    2:"b"
+}
+
+const obj2 = {
+    3:"c",
+    4:"d"
+}
+
+const obj4 = {
+    5:"e",
+    6:"f"
+}
+// const obj3 = {obj1 , obj2}
+
+// copy all object in obj3 
+// {} empty object target  object and others are source
+// const obj3 = Object.assign({},obj1,obj2, obj4)
+// console.log(obj3);
+
+const obj3 = {...obj1,...obj2,...obj4}
+
+// console.log(obj3);
+
+const users = [
+    {
+        userId:1,
+        email:"pravin@gmail",
+        profile:"user"
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+
+
+// users[1].email
+
+console.log(tinderUser);
+
+// return all keys of object but in array  
+console.log(Object.keys(tinderUser));
+
+// return all values of object but in array  
+console.log(Object.values(tinderUser));
+
+console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
