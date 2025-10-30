083ac7f1d00b851ad39d6ea3872098de5f2b8d3f
MrPravinS <shegamwarpravin07@gmail.com>
2025-10-30 09:57:43 +0530

    learn about hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,13 +5,57 @@ if(true){  // block scope
     let a = 300;
     const b = 200;
     var c = 43
-    console.log("inner :", a);
+    // console.log("inner :", a);
     
 
 }
-console.log("outer", a);
+// console.log("outer", a);
 
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+
+function one(){
+    const username = "pravin"
+    
+
+    function two(){
+        let website = "youtube"
+        console.log(username); // child variable can access paarent varaible
+        
+    }
+    // console.log(website); // parent function cant access child variable
+    two()
+}
+
+// one()
+
+if(true){
+    const username= "pravin";
+
+    if(username === "pravin"){
+        const website = " youtube"
+        // console.log(username + website); // pravin youtube
+        
+    }
+
+    // console.log(website); // give error
+    
+}
+    // console.log(username); // give error
+
+    // ++++++++++++++++ interesting +++++++++++++++
+
+    console.log(addOne(5))
+    function addOne(num){
+        return num + 1
+    }
+
+
+
+    addTwo(3)  // cant access before initialization becaus function is stroe in variable
+    const addTwo = function(num){  // expression
+     return num + 3
+    }
